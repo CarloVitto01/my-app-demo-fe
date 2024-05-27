3adc64e96d0ce7b225a8ba62c64aa7f2aeafaffe
Carlo <carlovittosc@gmail.com>
2024-05-27 23:46:33 +0200

    fixed service

--- src/service/SegnalazioniService.tsx
+++ src/service/SegnalazioniService.tsx
@@ -34,8 +34,8 @@ const filteredSegnalazioneBy = async (cognomeInput: string | null, dataInput: Da
             {
                 params:
                 {
-                    cognome: cognomeInput ?? '',
-                    data: dataInput ? dataInput.toString() : ''
+                    cognome: cognomeInput ? cognomeInput: null,
+                    data: dataInput ? dataInput : null
                 }
             }
         );
